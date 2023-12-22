090e484e8d9629c45a5b475a6379d247560de6d6
dima <dimanexren@gmail.com>
2023-12-22 22:41:52 +0300

    add handler

--- src/main/kotlin/com/example/demo/controller/TestController.kt
+++ src/main/kotlin/com/example/demo/controller/TestController.kt
@@ -3,6 +3,7 @@ package com.example.demo.controller
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.PathVariable
 
 data class Cat(val name: String, val age: Int)
 
@@ -11,6 +12,10 @@ class TestController {
 
     @GetMapping("/test1")
     fun getCat(@RequestParam name: String, @RequestParam age: Int): Cat {
+        return Cat(name, age)
+    }
+    @GetMapping("/cat/{name}/{age}")
+    fun getCatByPath(@PathVariable name: String, @PathVariable age: Int): Cat {
         return Cat(name, age)
     }
 }
