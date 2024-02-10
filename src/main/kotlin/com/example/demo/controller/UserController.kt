47c492fec5ddd3fbf8779b8907731b1522a334cc
Dmitry000XY <iatcenko.dv@gmail.com>
2024-02-10 23:36:43 +0300

    Preliminary commit.

--- src/main/kotlin/com/example/demo/controller/UserController.kt
+++ src/main/kotlin/com/example/demo/controller/UserController.kt
@@ -1,11 +1,29 @@
 package com.example.demo.controller
 
+import com.example.demo.data.User
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
 
+var people: MutableList<User> = mutableListOf()
+
+fun isInPeople(name: String): Boolean {
+    for (user in people) if (user.name == name) return true
+    return false
+}
+
 @RestController
 class UserController {
     @PostMapping("/user")
-    fun create(@RequestBody user: User) = user
+    fun create(@RequestBody user: User): ResponseEntity<Any> {
+        if (isInPeople(user.name)) {
+            return ResponseEntity<Any>(null, HttpStatus.CONFLICT)
+        } else {
+            val newUser = User(people.size, user.name, user.password)
+            people.add(newUser)
+            return ResponseEntity<Any>(newUser, HttpStatus.OK)
+        }
+    }
 }
---
+++ src/main/kotlin/com/example/demo/data/User.kt
@@ -0,0 +1,3 @@
+package com.example.demo.data
+
+data class User(val id: Int? = null, val name: String, val password: String)
