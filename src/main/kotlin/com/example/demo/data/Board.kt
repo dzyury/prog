1ab962376143128fe9f5865564c21440b75404a6
dzyury <dzyury@users.noreply.github.com>
2024-02-18 21:29:31 +0300

    Error Handler & Resources

---
+++ src/main/kotlin/com/example/demo/data/Board.kt
@@ -0,0 +1,9 @@
+package com.example.demo.data
+
+data class Board(
+    val id: Int,
+    val turn: Position,
+    val status: BoardStatus,
+    val details: String,
+    val users: List<User>
+)
---
+++ src/main/kotlin/com/example/demo/data/User.kt
@@ -0,0 +1,7 @@
+package com.example.demo.data
+
+data class User(
+    val name: String,
+    val role: Role,
+    val position: Position
+)
